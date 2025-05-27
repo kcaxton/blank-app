2a81bac643ae2bd2d0e98cbf1771b91e70ce1dde
keiceey <kiptoo.caxton@strathmore.edu>
2025-05-27 21:03:58 +0300

    project 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Title: Bio Data")
+st.write("This is sample web app.")
+
+first_name = st.text_input("First Name")
+last_name = st.text_input("Last Name")
+gender = st.selectbox("gender", ["Male", "Female"])
+age = st.number_input("Your Age", 0, 100, 30, 1)
+dob = st.date_input("Your Birthday")
+marital_cousin = st.radio("Marital Status", ["Single", "Married"])
+years_of_experience = st.slider("Years of experience", 0, 40)
+
+
+
